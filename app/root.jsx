1a6ec68aa4c32c43b32bd4827083c817fe703781
Badol-hossain <badol@iiinigence.com>
2023-04-15 16:05:32 +0600

    tailwind css added

--- app/root.jsx
+++ app/root.jsx
@@ -6,7 +6,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
+import styles from './styles/tailwind.css'
+export function links (){
+  return [{rel: 'stylesheet',href:styles}]
+}
 export default function App() {
   return (
     <html lang="en">
